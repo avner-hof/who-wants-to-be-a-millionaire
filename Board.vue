e0d3d1f3dd30d06a82e63a567f0e322a076077c4
avner-hof <75042623+avner-hof@users.noreply.github.com>
2020-11-25 21:42:59 +0200

    Add files via upload

---
+++ Board.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="content-container board-container">
+    <div class="header">
+      <div class="start-game-btn" @click="updateShowBoard(false)">Start Game</div>
+    </div>
+    <h2 class="title">LEADER BOARD</h2>
+    <div class="users-wrapper">
+      <div v-for="(boardScore, index) in boardScores" :key="index" class="user-score">
+        <span>{{ boardScore.username }}</span>
+        <span>{{ boardScore.totalScore }}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex';
+
+export default {
+  name: "Board",
+
+  computed: {
+    ...mapGetters(['boardScores'])
+  },
+
+  methods: {
+    ...mapActions(['updateShowBoard'])
+  }
+}
+</script>
+
+<style scoped>
+.board-container {
+  background-color: #2D2F31;
+  height: 100vh;
+}
+
+.start-game-btn {
+  padding: 10px;
+  width: 200px;
+  background-color: transparent;
+  color: rgba(255, 255, 255, 0.5);
+  border: thick solid rgba(255, 255, 255, 0.5);
+  border-radius: 20px;
+  margin: 25px 25px
+}
+
+.start-game-btn:hover {
+  border-color: rgb(255, 255, 255);
+  color: rgb(255, 255, 255);
+  cursor: pointer;
+}
+
+.title {
+  color: rgb(254, 247, 166);
+  margin-bottom: 30px;
+}
+
+.users-wrapper {
+  width: 100%;
+  height: 100%;
+  margin-bottom: 30px;
+  overflow-y: scroll;
+}
+
+.user-score {
+  display: grid;
+  grid-template-columns: 50% 50%;
+  align-items: start;
+  color: rgba(254, 247, 166, 0.9);
+  font-size: 32px;
+}
+
+</style>
---
+++ Modal.vue
@@ -0,0 +1,124 @@
+<template>
+  <transition name="modal">
+    <div class="modal-mask">
+      <div class="modal-wrapper">
+        <div class="modal-container">
+
+          <div class="modal-header">
+            <slot name="header"></slot>
+          </div>
+
+          <div class="modal-body">
+            <slot></slot>
+          </div>
+
+          <div class="modal-footer">
+            <slot name="footer">
+              <button class="modal-default-button" :disabled="isDisabled" @click="$emit('close')">
+                {{ buttonText }}
+              </button>
+            </slot>
+          </div>
+        </div>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+  export default {
+    name: 'Modal',
+
+    props: {
+      buttonText: {
+        type: String,
+        required: false,
+        default: 'OK'
+      },
+
+      isDisabled: {
+        type: Boolean,
+        required: false
+      }
+    }
+  }
+</script>
+
+<style scoped>
+.modal-mask {
+  position: fixed;
+  z-index: 9998;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: table;
+  transition: opacity 0.3s ease;
+}
+
+.modal-wrapper {
+  display: table-cell;
+  vertical-align: middle;
+}
+
+.modal-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  width: 400px;
+  margin: 0 auto;
+  padding: 20px 30px;
+  background-color: #000000;
+  border-radius: 2px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+  transition: all 0.3s ease;
+  font-family: OpenSans, Helvetica, Arial, sans-serif;
+}
+
+.modal-body {
+  margin: 20px 0;
+}
+
+.modal-default-button {
+  height: 69px;
+  width: 150px;
+  border-radius: 100px;
+  border-width: 0;
+  margin-right: 10px;
+  font-size: 30px;
+  font-weight: 500;
+  letter-spacing: 0;
+  font-family: inherit;
+  text-align: center;
+  background-color: #FF8F48;
+  color: #FFFFFF;
+}
+
+.modal-default-button:focus {
+  outline: none;
+}
+
+.modal-default-button:disabled {
+  background-color: #F0F0F0;
+}
+
+.modal-default-button:hover:enabled {
+  background-color: #F68034;
+}
+
+.modal-enter {
+  opacity: 0;
+}
+
+.modal-leave-active {
+  opacity: 0;
+}
+
+.modal-enter .modal-container,
+.modal-leave-active .modal-container {
+  -webkit-transform: scale(1.1);
+  transform: scale(1.1);
+}
+</style>
